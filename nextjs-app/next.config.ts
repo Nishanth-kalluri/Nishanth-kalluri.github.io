29901f90875f63da63a65395004deb7b96fa62c4
Nishanth-kalluri <kallurinishanth0@gmail.com>
2024-12-08 00:06:08 -0500

    changes in configurations for pages

--- nextjs-app/next.config.ts
+++ nextjs-app/next.config.ts
@@ -1,7 +1,10 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: "export", // Enables static HTML export
+  trailingSlash: true, // Adds trailing slashes to paths
+  assetPrefix: "/Nishanth-kalluri.github.io/", // Use the repository name
+  basePath: "/Nishanth-kalluri.github.io", // Set the base path to match GitHub Pages' structure
 };
 
 export default nextConfig;
--- nextjs-app/src/app/components/Section.tsx
+++ nextjs-app/src/app/components/Section.tsx
@@ -1,13 +1,15 @@
 import { ReactNode } from "react";
 
 interface SectionProps {
+  id: string; // Add the id prop
   title: string;
   children: ReactNode;
+  className?: string; // Make className optional
 }
 
-const Section = ({ title, children }: SectionProps) => {
+const Section = ({ id, title, children, className }: SectionProps) => {
   return (
-    <section className="container mx-auto py-8">
+    <section id={id} className={`container mx-auto py-8 ${className || ""}`}>
       <h2 className="text-2xl font-bold text-gray-800 dark:text-white mb-4">{title}</h2>
       <div className="text-gray-600 dark:text-gray-300">{children}</div>
     </section>
