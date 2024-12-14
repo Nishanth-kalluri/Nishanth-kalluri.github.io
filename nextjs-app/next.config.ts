0ff4e13f02e0076234ab853da6ba9d90fcc8f92e
Nishanth-kalluri <kallurinishanth0@gmail.com>
2024-12-13 22:22:57 -0500

    changed configurations of assetprefix

--- nextjs-app/next.config.ts
+++ nextjs-app/next.config.ts
@@ -5,8 +5,9 @@ const isProd = process.env.NODE_ENV === "production";
 const nextConfig: NextConfig = {
   output: isProd ? "export" : undefined, // Enable static export only for production
   trailingSlash: true, // Adds trailing slashes to paths
-  assetPrefix: isProd ? "/nextjs-app/" : undefined, // Use the repository name in production
-  basePath: isProd ? "/nextjs-app" : undefined, // Set the base path only for production
-};
+  assetPrefix: isProd ? "/" : undefined, // Use repository name in production
+  basePath: isProd ? "" : undefined // Set base path only for production
+
+  };
 
 export default nextConfig;
